a2fd9efa1712dc080ea40bcdfeae8547b194f7e0
ChrisScheidig <c@scheidig.net>
2017-06-21 20:38:40 +0200

    More tests

--- js/main.js
+++ js/main.js
@@ -1,42 +1,68 @@
 
-window.onload = init();
-var x_pos = 0,
-    y_pos = 0;
+define(function (require) {
 
-function createBox(x,y) {
-    let playground = document.getElementById('playground');
-    let newObject = document.createElement('div');
-    newObject.className = 'dragtest';
-    newObject.style.position = 'absolute';
-    newObject.style.top = y;
-    newObject.style.left = x;
-    playground.appendChild(newObject);
-    return newObject;
-}
+    //window.onload = init();
+    var x_pos = 0,
+        y_pos = 0;
 
-function addDragFunctionality(obj) {
+    var objGen = require('./objectGenerator');
+    var makeDraggable = require('./draggable');
 
-    let divMove = function(e) {
-        obj.style.position = 'absolute';
-        obj.style.top = (e.clientY - y_pos) + 'px';
-        obj.style.left = (e.clientX - x_pos) + 'px';
+    let playfield = {
+        objects:[]
     }
-    let mouseDown = function(e) {
-        x_pos = e.clientX - obj.offsetLeft;
-        y_pos = e.clientY - obj.offsetTop;
-        window.addEventListener('mousemove',divMove,true);
+
+    function init() {
+        counter = 0;
+
+        let obj1 = objGen.createObject( {
+            type:"card",x:100,y:100,
+            /// Content ist abhängig vom Objecttyp!
+            content: {
+                text:"eine ganz besondere Karte"
+            }
+        });
+        makeDraggable(obj1);
+
+        let obj2 = objGen.createObject( {
+            type:"chip",x:300,y:100,
+            /// Content ist abhängig vom Objecttyp!
+            content: {
+            }
+        });
+        makeDraggable(obj2);
+
+        let obj3 = objGen.createObject( {
+            type:"card",x:200,y:300,
+            /// Content ist abhängig vom Objecttyp!
+            content: {
+                text:"Eine langweilige Karte"
+            }
+        });
+        makeDraggable(obj3);
+ 
+        let obj4 = objGen.createObject( {
+            type:"card",x:250,y:300,
+            /// Content ist abhängig vom Objecttyp!
+            content: {
+                text:"SCHATZKARTE???"
+            }
+        });
+        makeDraggable(obj4);
+
+
+        var testObj = {
+            value:1,
+            objects: [
+                {id:2},
+                {id:3},
+                {id:4}
+            ]
+        }
+
+        console.log( JSON.stringify(testObj));
+
     }
-    let mouseUp = function() {
-        window.removeEventListener('mousemove',divMove,true);
-    }   
 
-    obj.addEventListener('mousedown',mouseDown,false);
-    window.addEventListener('mouseup',mouseUp, false);
-    return obj;
-}
-
-function init() {
-    addDragFunctionality(createBox(100,100));
-    addDragFunctionality(createBox(300,100));  
-    addDragFunctionality(createBox(200,300));    
-}
+    init();
+});
